2ef23562a0474cea1b46825bcdc466d605ba96e1
Thijs Eilander <eilander@myguard.nl>
2023-12-01 00:53:44 +0100

    made it working :-)

--- scripts/script.sh
+++ scripts/script.sh
@@ -3,16 +3,20 @@
 FILE="botmap.conf"
 ROBOTSTXT="robots.txt"
 
-LIST=$(wget -q -O - https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/_generator_lists/bad-user-agents.list)
+wget -q -O - https://raw.githubusercontent.com/mitchellkrogza/nginx-ultimate-bad-bot-blocker/master/_generator_lists/bad-user-agents.list > bad-user-agents.list
 
 echo "map \$http_user_agent \$bad_client { " > ${FILE}
 echo -e "\tdefault 0;" >> ${FILE}
-for ENTRY in $LIST; do
+
+while read -r ENTRY;
+do
 	echo -e "\t${ENTRY} 1;" >> ${FILE}
 	echo "User-agent: ${ENTRY}" >> ${ROBOTSTXT}
 	echo "Disallow: /" >> ${ROBOTSTXT}
+done < bad-user-agents.list
 
-done
 echo "}" >> ${FILE}
 
+sed -i s/"\\\ "/" "/g ${ROBOTSTXT}
+sed -i s/"\\\ "/" "/g ${FILE}
 
